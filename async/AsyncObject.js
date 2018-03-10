239d6a6d5373c926bd2a865a3efd6291d6eb37d7
gismayilov <gismayilov@griddynamics.com>
2018-03-10 16:02:10 +0300

    AsyncTree object

--- async/AsyncObject.js
+++ async/AsyncObject.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const AsyncTree = require('./AsyncTree');
 const TreeNode = require('./TreeNode');
 const AsyncTreeNode = require('./AsyncTreeNode');
 const NotDefinedAsyncTreeNode = require('./NotDefinedAsyncTreeNode');
@@ -10,56 +11,31 @@ class AsyncObject {
     this.args = args;
   }
 
-  // it needs to be overridden
+  /****** To be overriden ******/
+
   definedAsyncCall() {
     return () => {};
   }
 
-  // it can be overridden
   onError(error) {
     throw error;
   }
 
-  /* 
-    it can be overridden,
-      must return the processed result 
-  */
   onResult(result) {
-    // no async calls here, just process the result
     return result;
   }
 
-  // api alias (for async tree root only)
+  /*****************************/
+
   call() {
     let nodes = [];
-    this.buildAsyncTreeNode(new NotDefinedAsyncTreeNode(), nodes, 0);
-    /*
-      leaf can be either simple argument or
-        just AsyncObject without any arguments 
-    */
-    let leaves = nodes.filter(node => {
-      return node.isLeave();
-    });
-    leaves.forEach(node => {
-      node.call();
-    });
+    new AsyncTree(this).create().call();
   }
 
-  buildAsyncTreeNode(parent, nodes, index) {
-    let asyncTreeNode = new AsyncTreeNode(this, parent, index);
-    nodes.push(asyncTreeNode);
+  iterateArgs(func) {
     this.args.forEach((arg, index) => {
-      if (arg instanceof AsyncObject) {
-        arg.buildAsyncTreeNode(asyncTreeNode, nodes, index);
-      } else {
-        this.buildTreeNode(arg, asyncTreeNode, nodes, index);
-      }
+      func(arg, index, arg instanceof AsyncObject);
     });
-  }
-
-  buildTreeNode(arg, parent, nodes, index) {
-    let treeNode = new TreeNode(arg, parent, index);
-    nodes.push(treeNode);
   }
 
   hasNoArgs() {
