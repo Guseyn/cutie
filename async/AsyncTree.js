a1fd0699004ec9b3e44f173adf12c848ae92e45e
gismayilov <gismayilov@griddynamics.com>
2018-03-10 16:02:10 +0300

    AsyncTree object

---
+++ async/AsyncTree.js
@@ -0,0 +1,50 @@
+'use strict'
+
+const TreeNode = require('./TreeNode');
+const AsyncTreeNode = require('./AsyncTreeNode');
+const NotDefinedAsyncTreeNode = require('./NotDefinedAsyncTreeNode');
+
+class AsyncTree {
+
+	/**
+		rootField: AsyncObject
+	**/
+	constructor(rootField) {
+		this.rootField = rootField;
+		this.nodes = [];
+	}
+
+	create() {
+		this.createAsyncTreeNode(this.rootField, new NotDefinedAsyncTreeNode(), 0);
+		return this;
+	}
+
+	call() {
+		let leaves = this.nodes.filter(node => {
+      return node.isLeave();
+    });
+    leaves.forEach(leaf => {
+      leaf.call();
+    });
+	}
+
+	createAsyncTreeNode(field, parent, index) {
+		let asyncTreeNode = new AsyncTreeNode(field, parent, index);
+    this.nodes.push(asyncTreeNode);
+    field.iterateArgs((arg, index, isArgAsync) => {
+    	if (isArgAsync) {
+    		this.createAsyncTreeNode(arg, asyncTreeNode, index);
+      } else {
+        this.createTreeNode(arg, asyncTreeNode, index);
+      }
+    });
+	}
+
+	createTreeNode(field, parent, index) {
+    let treeNode = new TreeNode(field, parent, index);
+    this.nodes.push(treeNode);
+	}
+
+}
+
+module.exports = AsyncTree;
