b8851f1425720cf652a6a320424738fb1594f208
gismayilov <gismayilov@griddynamics.com>
2018-03-11 13:04:09 +0300

    Add simple test

---
+++ test/SyncMaxNum.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const AsyncObject = require('./../src/AsyncObject');
+
+class SyncMaxNum extends AsyncObject {
+
+	constructor(a, b, c) {
+    super(a, b, c);
+  }
+
+	definedSyncCall() {
+		return (a, b, c) => {
+			return Math.max(a, b, c);
+		}
+	}
+
+}
+
+module.exports = SyncMaxNum;
