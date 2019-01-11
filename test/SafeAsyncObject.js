f1e73ec5c9d8fee3760a16509f6ddea16a6b81cf
gismayilov <guseynism@gmail.com>
2019-01-11 19:43:20 +0300

    improvment coverage(1st step)

---
+++ test/SafeAsyncObject.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const AsyncObject = require('./../src/AsyncObject')
+
+class SafeAsyncObject extends AsyncObject {
+  constructor () {
+    super()
+  }
+
+  definedSyncCall() {
+    return () => {
+      throw new Error('safe error')
+    }
+  }
+
+  continueAfterFail() {
+    return true
+  }
+}
+
+module.exports = SafeAsyncObject
