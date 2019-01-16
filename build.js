737bc6bb5f0371bb7675f641acb53ab7ef1835ea
gismayilov <guseynism@gmail.com>
2019-01-16 23:48:21 +0300

    total coverage

--- build.js
+++ build.js
@@ -4,16 +4,33 @@ const {
   ExecutedLint,
   ExecutedTestCoverage,
   ExecutedTestCoverageCheck,
-  ExecutedTestCoverageReport
+  ExecutedTestCoverageReport,
+  LoggedTotalCoverageByJsonSummary
 } = require('@cuties/wall')
+const {
+  ReadDataByPath
+} = require('@cuties/fs')
+const {
+  ParsedJSON
+} = require('@cuties/json')
 
 new ExecutedLint(process, './src').after(
   new ExecutedLint(process, './test').after(
-    new ExecutedTestCoverageCheck(
-      new ExecutedTestCoverage(
-        process, './test/test.js'
-      ),
-      { 'lines': 100, 'functions': 100, 'branches': 100 }
+    new ExecutedTestCoverageReport(
+      new ExecutedTestCoverageCheck(
+        new ExecutedTestCoverage(
+          process, './test/test.js'
+        ),
+        { 'lines': 100, 'functions': 100, 'branches': 100 }
+      ), 'json-summary'
+    ).after(
+      new LoggedTotalCoverageByJsonSummary(
+        new ParsedJSON(
+          new ReadDataByPath('coverage/coverage-summary.json', { encoding: 'utf8' })
+        ), (linesPct, statementsPct, functionsPct, branchesPct) => {
+          return (linesPct + statementsPct + functionsPct + branchesPct) / 4
+        }
+      )
     )
   )
 ).call()
